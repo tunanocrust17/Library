372495a55a8d8088772665b617d436eeb0cfac56
Eric Huet <ewhuet@gmail.com>
2023-12-05 15:30:17 +0200

    added in a new object constructor to create a new book with a title, author, pages, and if it was read yet, tested it adding The Hobbit and console logged the info function

---
+++ app.js
@@ -0,0 +1,13 @@
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+       return this.title + " by " + this.author + ", " + this.pages + ", " + this.read
+    };
+}
+
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', "not read yet");
+
+console.log(book1.info());
