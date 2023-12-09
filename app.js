16a208f2315abb0b5b9c242fa8ecb6071be14f53
Eric Huet <ewhuet@gmail.com>
2023-12-09 15:41:08 +0200

    mapped the add-book-btn, added a function addBookToLibrary which creates a new Book object using inputs from the user and then pushes it into the myLibrary array, added the addbooktolibrary function to the addbookbutton click method

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const dialog = document.querySelector('dialog');
 const showButton = document.querySelector('.open-dialog');
 const cancelButton = document.querySelector('.cancel-btn');
+const addBookButton = document.querySelector('.add-book-btn');
 
 showButton.addEventListener("click", () => {
     dialog.showModal();
@@ -23,5 +24,25 @@ function Book(title, author, pages, read){
 }
 
 const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', "not read yet");
+myLibrary.push(book1);
+console.log(myLibrary);
 
-console.log(book1.info());
+function addBookToLibrary(){
+    let newBook = new Book (
+        document.getElementById('book-title').value,
+        document.getElementById('book-author').value,
+        document.getElementById('book-pages').value,
+        document.getElementById('book-read').value);
+myLibrary.push(newBook);
+    }
+
+
+addBookButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    // console.log(document.getElementById('book-title').value);
+    addBookToLibrary();
+    console.log(myLibrary)
+    dialog.close();
+    }
+
+)
